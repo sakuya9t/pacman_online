96f7dc1238ab3216eaad9e029fd83fbd525ffaa9
Leewei222 <38178346+Leewei222@users.noreply.github.com>
2019-04-19 01:14:42 +1000

    add multicast framework. TODO sequencer

--- networking/messageHandler.py
+++ networking/messageHandler.py
@@ -16,6 +16,13 @@ class messageHandler(threading.Thread):
         self.b2_queue = Queue()
         self.logger = logger
 
+        # holdback queue
+        self.r1_hold_q = Queue()
+        self.r2_hold_q = Queue()
+        self.b1_hold_q = Queue()
+        self.b2_hold_q = Queue()
+        self.p_seq = 0  # process sequence number
+
     def run(self):
         while True:
             time.sleep(0.1)
@@ -25,14 +32,76 @@ class messageHandler(threading.Thread):
             try:
                 source_ip, source_port = msg['ip'], msg['port']
                 msg = json.loads(msg['message'])
-                agent = msg['agent']
-                if agent == 'R1':
-                    self.r1_queue.push(msg['direction'])
-                if agent == 'B1':
-                    self.b1_queue.push(msg['direction'])
-                if agent == 'R2':
-                    self.r2_queue.push(msg['direction'])
-                if agent == 'B2':
-                    self.b2_queue.push(msg['direction'])
+
+                command = ""
+                if 'command' in msg:
+                    command = msg['command']
+                if command == "holdback":
+                    key = msg['key']
+                    agent, msg_count = msg['msg_id']
+                    if agent == 'R1':
+                        self.r1_hold_q.push((msg_count, key))
+                    elif agent == 'B1':
+                        self.b1_hold_q.push((msg_count, key))
+                    elif agent == 'R2':
+                        self.r2_hold_q.push((msg_count, key))
+                    elif agent == 'B2':
+                        self.b2_hold_q.push((msg_count, key))
+                elif command == "order":
+                    agent, msg_count = msg['msg_id']
+                    g_seq = msg['group_sequence']
+                    # assume the messages from sequencer are ordered for now
+                    if self.p_seq == g_seq:
+                        self.order(agent, msg_count)
+                        self.p_seq += 1
+                    else:
+                        pass
+
+                else:
+                    agent = msg['agent']
+                    if agent == 'R1':
+                        self.r1_queue.push(msg['direction'])
+                    if agent == 'B1':
+                        self.b1_queue.push(msg['direction'])
+                    if agent == 'R2':
+                        self.r2_queue.push(msg['direction'])
+                    if agent == 'B2':
+                        self.b2_queue.push(msg['direction'])
             except Exception as e:
                 self.logger.error(str(e))
+
+    def order(self, agent, msg_count):
+        if agent == 'R1':
+            id, key = self.r1_hold_q.pop()
+            # delete garbage msg
+            while id != msg_count:
+                id, key = self.r1_hold_q.pop()
+            # deliver msg
+            if id == msg_count:
+                self.r1_queue.push(key)
+            else:
+                self.logger.error("messageHandler error: message not in queue")
+        if agent == 'B1':
+            id, key = self.b1_hold_q.pop()
+            while id != msg_count:
+                id, key = self.b1_hold_q.pop()
+            if id == msg_count:
+                self.b1_queue.push(key)
+            else:
+                self.logger.error("messageHandler error: message not in queue")
+        if agent == 'R2':
+            id, key = self.r2_hold_q.pop()
+            while id != msg_count:
+                id, key = self.r2_hold_q.pop()
+            if id == msg_count:
+                self.r2_queue.push(key)
+            else:
+                self.logger.error("messageHandler error: message not in queue")
+        if agent == 'B2':
+            id, key = self.b2_hold_q.pop()
+            while id != msg_count:
+                id, key = self.b2_hold_q.pop()
+            if id == msg_count:
+                self.b2_queue.push(key)
+            else:
+                self.logger.error("messageHandler error: message not in queue")
