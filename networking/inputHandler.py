778281d49b52ecff5eca87ff65b99d094f8dd38d
Leewei222 <38178346+Leewei222@users.noreply.github.com>
2019-04-19 01:10:28 +1000

    multicast framework: TODO cannot connect to server

--- networking/inputHandler.py
+++ networking/inputHandler.py
@@ -14,8 +14,18 @@ class inputHandler(threading.Thread):
         self.buffer = server.input_queue
         keyboard.on_press(self.key_press)
         self.msgbuffer = ""
+        # how to get the agent?
+        self.agent = 'B1'  # hardcode to 'b1' for now
+        self.msg_count = 0  # used with agent as message id
+        self.recv_q = server.recv_queue
+
+        self.server_ip = server.ip
+        self.server_port = server.port
 
     def run(self):
+        # TODO cannot connect.....
+        # self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # self.conn.connect((self.server_ip, self.server_port))
         while True:
             time.sleep(500)
 
@@ -26,6 +36,13 @@ class inputHandler(threading.Thread):
             return
         if key_name in ['up', 'down', 'left', 'right']:
             self.buffer.push({'key': key_name})
+            # multicast to all other players, just send to self for now....
+            # logger.info(json.dumps({"command": "holdback", "key": key_name,
+            #                         "msg_id": (self.agent, self.msg_count)}))
+            # self.conn.sendall(json.dumps(
+            #     {"command": "holdback", "key": key_name, "msg_id": (self.agent, self.msg_count)}))
+            # self.msg_count += 1
+            
         if key.name == u'enter':
             self.buffer.push({'msg': self.msgbuffer})
             self.msgbuffer = ""
