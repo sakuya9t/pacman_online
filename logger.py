f048098295db646e42035bdddd0d574837c8233e
unknown <naiw1@student.unimelb.edu.au>
2019-04-24 22:28:40 +1000

    clear out

--- logger.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-
-def info(message):
-    time = str(datetime.datetime.now().strftime("[%Y-%m-%d %H:%M:%S]"))
-    message = str(message)
-    print "(INFO) " + time + " " + message
